994096a06de5779bb772f0a944bf24c6debd6352
Danny Collan <collan97@gmail.com>
2023-12-16 21:51:04 -0800

    Exported images for applications and projects

---
+++ src/assets/images/index.js
@@ -0,0 +1,35 @@
+import workdayscheduler from "./workdayscheduler.png";
+import fantasticweatherapp from "./fantasticweatherapp.png";
+import quizbiz from "./quizbiz.png";
+import expressnotetaker from "./expressnotetaker.png";
+import uniquesqlemployeetracker from "./uniquesqlemployeetracker.png";
+import groovyreadmegenerator from "./groovyreadmegenerator.png";
+import horiseon from "./horiseon.png";
+import supersvggenerator from "./supersvggenerator.png";
+import uploadedFileLink from "./A1.pdf";
+import excellentpasswordgenerator from "./excellentpasswordgenerator.png";
+import joyfindr from "./joyfindr.png";
+import funkyormapp from "./funkyormapp.png";
+import truetechblog from "./truetechblog.png";
+import nosqlsocialnetworkapi from "./nosqlsocialnetworkapi.png";
+import fabulouspwatexteditor from "./fabulouspwatexteditor.png";
+
+
+export {
+
+    workdayscheduler,
+    fantasticweatherapp,
+    quizbiz,
+    expressnotetaker,
+    uniquesqlemployeetracker,
+    groovyreadmegenerator,
+    horiseon,
+    supersvggenerator,
+    uploadedFileLink,
+    excellentpasswordgenerator,
+    joyfindr,
+    funkyormapp,
+    truetechblog,
+    nosqlsocialnetworkapi,
+    fabulouspwatexteditor
+};
