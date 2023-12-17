17d8139976594ab2c315d6cdc083e3a74173c65d
Danny Collan <collan97@gmail.com>
2023-12-16 21:04:31 -0800

    Initialize Repo, setup React files with additional ones

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./components/About";
+import Applications from "./components/Applications";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+import Particles from 'react-tsparticles';
+import {loadStarsPreset} from "tsparticles-preset-stars";
 
+class ParticlesContainer extends React.PureComponent {
+  async customInit(engine){
+    await loadStarsPreset(engine);
+  }
+  render() {
+    const options = {
+      preset: "stars"
+    };
+    return <Particles options={options}init={this.customInit}style={{opacity: "0.2"}}/>
+  }
+}
 function App() {
+  const [selectedPage, setSelectedPage] = useState("About");
+
+  const changePage = (page) => {
+    setSelectedPage(page);
+  };
+
+  const renderPage = () => {
+    switch (selectedPage) {
+      case "About":
+        return <About />;
+      case "Applications":
+        return <Applications />;
+      case "Projects":
+        return <Projects />;
+      case "Resume":
+        return <Resume />;
+      case "Contact":
+        return <Contact />;
+      default:
+        return <h1>Page not found!</h1>;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar changePage={changePage} />
+      <ParticlesContainer/>
+      {renderPage()}
+      <Footer />
+    </>
   );
 }
 
