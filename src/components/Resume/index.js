ece753c6b963cd8cce63375143ed7c1bbc3a452a
Danny Collan <collan97@gmail.com>
2024-01-14 16:07:12 -0800

    Updated Resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,30 +8,53 @@ const Resume = () => {
     <>
       <div className="resume-sec">
         <h1 className="skill">Skills</h1>
+        <h3 className="skill">Programming Languages</h3>
+        <div>
+          <ul className="skills-section1">
+            <li>Java</li>
+            <li>C</li>
+            <li>C++</li>
+            <li>Java</li>
+            <li>Python</li>
+            <li>JavaScript</li>
+          </ul>
+        </div>
         <h3 className="skill">Front-end</h3>
         <div>
           <ul className="skills-section1">
-            <li>HTML</li>
+            <li>HTML5</li>
             <li>CSS</li>
-            <li>JavaScript</li>
-            <li>jQuery</li>
-            <li>Responsive Web design</li>
+            <li>XML</li>
             <li>React.js</li>
             <li>Bootstrap</li>
+            <li>Responsive Web design</li>
+            <li>jQuery</li>
           </ul>
         </div>
         <h3 className="skill">Back-end</h3>
         <div>
           <ul className="skills-section1">
-            <li>Java</li>
-            <li>C/C++</li>
-            <li>Python</li>
-            <li>REST APIs</li>
-            <li>Node.js</li>
-            <li>Express.js</li>
             <li>MySQL</li>
             <li>MongoDB</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>REST APIs</li>
             <li>GraphQL</li>
+          </ul>
+        </div>
+        <h3 className="skill">Methodologies</h3>
+        <div>
+          <ul className="skills-section1">
+            <li>Agile development</li>
+            <li>Data Structures</li>
+            <li>Algorithms</li>
+          </ul>
+        </div>
+        <h3 className="skill">Version Control</h3>
+        <div>
+          <ul className="skills-section1">
+            <li>Git</li>
+            <li>Github</li>
           </ul>
         </div>
 
